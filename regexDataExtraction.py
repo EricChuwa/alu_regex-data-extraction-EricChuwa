83a8e6f70cbf81719487435c6f75b70574af9f97
EricChuwa <eric.c.chuwa@gmail.com>
2026-02-02 21:11:39 +0200

    Switching to Python

---
+++ regexDataExtraction.py
@@ -0,0 +1,31 @@
+import re
+
+class BaseValidator:
+    pass
+
+class EmailValidator:
+    pass
+
+class PhoneValidator:
+    pass
+
+class URLValidator:
+    pass
+
+class CreditCardNumberValidator:
+    pass
+
+class TimeValidator:
+    pass
+
+class HtmlTagsValidator:
+    pass
+
+class HashtagsValidator:
+    pass
+
+class CurrencyValidator:
+    pass
+
+class DataExtractor:
+    pass
